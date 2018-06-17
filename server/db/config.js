b5117599f9bdb0a3888edbadcdde007501e01947
Patrick Rivera <pxr13@my.fsu.edu>
2018-06-17 19:47:17 -0400

    Changed postinstall

--- server/db/config.js
+++ server/db/config.js
@@ -3,6 +3,6 @@ const configuration = require('../../knexfile')[environment];
 
 const db = require('knex')(configuration);
 
-// db.migrate.latest([configuration]);
+db.migrate.latest([configuration]);
 
 module.exports = db;
