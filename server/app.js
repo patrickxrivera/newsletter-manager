50b969ba3002a317ff321d82a960bcfa9eba9e06
Patrick Rivera <pxr13@my.fsu.edu>
2018-06-18 12:33:29 -0400

    Added html for verification

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
+app.use('/', express.static(__dirname + '/html'));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
