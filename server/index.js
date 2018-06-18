d6428e9886b40a668bc8bd4c04cc68229841826a
Patrick Rivera <pxr13@my.fsu.edu>
2018-06-17 20:09:16 -0400

    Removed client stuff

--- server/index.js
+++ server/index.js
@@ -5,22 +5,22 @@ const { Client } = require('pg');
 
 const port = process.env.PORT || 8080;
 
-if (process.env.NODE_ENV === 'production') {
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-  });
-
-  client.connect();
-
-  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   const client = new Client({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: true
+//   });
+//
+//   client.connect();
+//
+//   client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//     if (err) throw err;
+//
+//     for (let row of res.rows) {
+//       console.log(JSON.stringify(row));
+//     }
+//     client.end();
+//   });
+// }
 
 app.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
