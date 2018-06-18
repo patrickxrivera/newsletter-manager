1ee5339279f526cfd3fce4fd2d893b86e4d8f542
Patrick Rivera <pxr13@my.fsu.edu>
2018-06-17 20:35:20 -0400

    Removed second migration

--- server/db/config.js
+++ server/db/config.js
@@ -1,8 +1,6 @@
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../knexfile')[environment];
 
-console.log(configuration);
-
 const db = require('knex')(configuration);
 
 module.exports = db;
